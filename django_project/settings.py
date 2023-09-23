8df0112ce32f8444fe3a6b7f25cc44ed16839567
salih <ahmad.72121@gmail.com>
2023-09-23 19:44:18 +0300

    add views for apis

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #local apps
     'todo.apps.TodoConfig',
+    #third party apps
+    'rest_framework',
+
 ]
 
-
+REST_FRAMEWORK = {
+ "DEFAULT_PERMISSION_CLASSES": [
+ "rest_framework.permissions.AllowAny",
+ ],
+}
 
 
 MIDDLEWARE = [
